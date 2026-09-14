953abe1587fc0fe780c90f7eeb6fc82e6e0f6dcd
pouya <pouyakhaleghi82@gmail.com>
2026-09-14 21:13:54 +0330

    feat:install react query for auth

---
+++ src/app/auth/SendOTPFrom.jsx
@@ -0,0 +1,26 @@
+import TextField from "@/common/TextField";
+
+function SendOTPFrom({ phoneNumber, onChange, onSubmit, isLoading }) {
+  return (
+    <div>
+      <form className="space-y-10" onSubmit={onSubmit}>
+        <TextField
+          label="شماره موبایل"
+          name="phoneNumber"
+          value={phoneNumber}
+          onChange={onChange}
+        />
+        <div>
+          {isLoading ? (
+            <p>hello</p>
+          ) : (
+            <button type="submit" className="btn btn--primary w-full">
+              ارسال کد تایید
+            </button>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+}
+export default SendOTPFrom;
