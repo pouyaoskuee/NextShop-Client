e8913f97578a5706ccaee3a5c92af8a33eb7341e
pouya <pouyakhaleghi82@gmail.com>
2026-09-22 23:11:19 +0330

    refactor:create rout groups

--- src/app/(home)/layout.js
+++ src/app/(home)/layout.js
@@ -1,4 +1,4 @@
-import "./globals.css";
+import "../../styles/globals.css";
 import vazirFont from "@/constants/localFonts";
 import Header from "@/app/Header";
 import {Toaster} from "react-hot-toast";
